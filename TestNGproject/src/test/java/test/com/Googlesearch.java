b7830d9d8bc56d161a1115aed6b99324cfc06fe2
Myroslava Choliy <48024349+MyroslavaCholiy@users.noreply.github.com>
2019-11-24 16:11:59 -0600

    init. file

---
+++ TestNGproject/src/test/java/test/com/Googlesearch.java
@@ -0,0 +1,49 @@
+
+package test.com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Googlesearch {
+    private static WebDriver driver;
+    private String baseUrl="https://www.google.com";  
+    
+    public Googlesearch() {
+        
+    }
+
+  
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+         WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "c:\\data1\\chromedriver.exe");
+         driver = new ChromeDriver();
+         driver.get(baseUrl);
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+        driver.quit();
+    }
+    
+    
+    @Test
+    public void testSearch(){
+    driver.findElement(By.xpath(".//*[@name='q']")).click();
+    driver.findElement(By.xpath(".//*[@name='q']")).clear();
+    driver.findElement(By.xpath(".//*[@name='q']")).sendKeys("Basketball");
+    driver.findElement(By.xpath(".//*[@name='q']")).submit();
+    }
+}
